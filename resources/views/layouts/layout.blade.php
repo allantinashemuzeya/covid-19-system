dd2e890454e02f9f9ed2dc28e62bc6c190f50689
Allan Muzeya <allan.muzeya@wundermanthompson.com>
2022-09-16 10:09:27 +0200

    initial dev commit

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Mirrored from nextpath.co.za/VerifyResults by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 01 Oct 2021 22:39:25 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="format-detection" content="telephone=no" />
+
+    <title>Verify Results | NextPath Registration Portal</title>
+
+
+
+    <link rel="stylesheet" href="{{asset('lib/fontawesome/css/all.min.css')}}" />
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&amp;family=Roboto:wght@300&amp;display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="{{asset('lib/dm-file-uploader/css/jquery.dm-uploader.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('lib/select2/css/select2.min.css')}}" />
+
+
+    <link rel="stylesheet" href="{{asset('css/site25e8.css?v=25BqWgxqBWj3SttJ38AUvXjzmMBVqsQj565Jge8CBnw')}}" />
+</head>
+
+<body class="bg-primary">
+
+    <nav class="navbar navbar-expand-md navbar-light bg-white">
+        <div class="container">
+            <a class="navbar-brand" href="Account/SignInfab1.html"><img src="images/logo-v3.png" alt="NextPath"
+                    style="width: 315px; height: 80px;"></a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarMain">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item text-right mt-5 mt-md-0">
+
+                        <a class="nav-link py-0 my-0" href="mailto:info@nextpluspathology.co.za"><i
+                                class="fas fa-envelope"></i> info@nextpath.co.za</a>
+                        <a class="nav-link py-0 my-0" href="tel:+27845899764"><i class="fas fa-phone"></i> 084 589
+                            9764</a>
+
+                    </li>
+                    <li class="nav-item">
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    @yield('content')
+    
+    <footer class="footer py-3 bg-primary text-white">
+        <div class="container">
+            <p class="small">&copy; 2021 NextPath</p>
+        </div>
+    </footer>
+
+
+
+    <script src="{{asset('lib/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('lib/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+
+    <script src="{{asset('js/site746d.js?v=47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU')}}"></script>
+
+
+    <script src="{{asset('lib/jquery-validation/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.min.js')}}"></script>
+    <script src="{{asset('lib/jquery-mask-plugin/jquery.mask.min.js')}}"></script>
+    <script src="{{asset('lib/select2/js/select2.min.js')}}"></script>
+
+
+    <script type="text/html" id="spinner-template">
+        <span class="spinner-border spinner-border-sm"></span>
+        Finding...
+    </script>
+
+    <script>
+        $(document).ready(function() {
+
+            $("form").data("validator")
+                .settings
+                .submitHandler = function(form) {
+
+                    var spinnerHtml = $('#spinner-template').html();
+                    var button = $(form).find('button[type="submit"]');
+
+                    button.html(spinnerHtml);
+                    button.prop('disabled', true);
+
+                    form.submit();
+                };
+        });
+    </script>
+
+</body>
+</html>
---
+++ verify/verify/resources/views/VerificationForm.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.layout') 
+@section('content')
+	
+	<div class="section bg-white">
+		<div class="section">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-12 col-md-4 col-lg-3 py-3">
+						<div class=" bg-light border p-3">
+							<h2 class="text-primary"><strong>Verify Results Process</strong></h2>
+							<ol class="steps">
+								<li class="steps-item active"><span>1</span> Patient Information</li>
+								<li class="steps-item "><span>2</span> Complete</li>
+							</ol>
+						</div>
+					</div>
+
+					<div class="col-12 col-md-8 col-lg-9 py-5">
+
+						<p class="mb-5">
+							<strong>Please enter the patient's information below.</strong>
+						</p>
+
+						<form autocomplete="off" action="{{ route('verifyWP') }}" method="post">
+						@csrf
+							<div class="form-row">
+
+								<div class="form-group col-12 col-lg-3">
+									<label for="IdentityNumber">Identity or Passport Number</label> *
+									<input required 
+										class="form-control"
+										type="text" data-val="true"
+										data-val-length="The field Identity or Passport Number must be a string with a maximum length of 30."
+										data-val-length-max="30"
+										data-val-required="The Identity or Passport Number field is required."
+										id="IdentityNumber"
+										maxlength="30"
+										name="IdentityNumber" 
+										value="CN794099" />
+									<span class="text-danger small field-validation-valid"
+										data-valmsg-for="IdentityNumber" data-valmsg-replace="true"></span>
+								</div>
+
+							</div>
+
+							
+							<div class="form-row mt-5 justify-content-between">
+								<div class="form-group col-6">
+
+								</div>
+								<div class="form-group col-6 text-right">
+									<button type="submit" class="btn btn-primary px-5">Find</button>
+								</div>
+							</div>
+
+							<input name="__RequestVerificationToken" type="hidden"
+								value="CfDJ8AMbY6i5bHRJtERDdtHduaCIZk_LNz7Xt7lfpXmHIkhvSd7Fm66msmOVJ_yWpNAu9kN6svmx-CZmGGX57hWC1b0OLN75qBj4BUPVNucWn_vwAWXXVaDtPlwVPxfd6LwBF4BgS2NSlLsWDo_3KwKnFig" />
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+@endsection
